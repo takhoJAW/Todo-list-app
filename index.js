885c48c9d8f38911d04231eba8a14250e521b056
Taha <gachetjaa@gmail.com>
2025-05-27 17:33:16 +0400

    Update database password and configuration

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 const port = 3000;
 
 const db = new pg.Client({
-    user: "postgres",
-    password: "tmwstw414",
-    host: "localhost",
-    database: "Permalist",
-    port: 5432,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
   });
   db.connect();
 
